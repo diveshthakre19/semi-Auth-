8bd85f0d29bec14cb1341655dcbd1631ebbea997
diveshthakre19 <diveshthakre07@gmail.com>
2022-12-01 14:09:26 +0530

    connected with database using mongoose

---
+++ auth/config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const MONGODB_URL = "theurlstring";
+// this is a method we have created exports.connect
+exports.connect = () => {
+  mongoose
+    .connect(MONGODB_URL, {
+      // we have to write this (default option in mongoDB)
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(console.log("Db connected sucessfully"))
+    .catch((error) => {
+      console.log("Db connection fail ");
+      console.log(error);
+      // read about this
+      process.exit(1);
+    });
+};
